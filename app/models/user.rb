65b3f530c76046adbc9931ab1c1b45d3a01b3102
Elaine Anderson <eparie@bu.edu>
2018-07-25 18:57:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ActiveRecord::Base
 
 has_secure_password
 
-def self.find_by_slug(slug)
-  self.all.find do |user|
-    user.slug == slug
-    end
-
   def slug
     user = self.username
     user = user.downcase
     user = user.split(" ")
     user.join("-")
   end
+
+  def self.find_by_slug(slug)
+    self.all.find do |user|
+      user.slug == slug
+      end
 end
 
 
